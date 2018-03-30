5e19e21640a4a827d9189db9f4f03e2f8a087c22
John Blischak <jdblischak@gmail.com>
2018-03-30 11:42:59 -0400

    Improve simulation for ch01.

--- code/ch01.R
+++ code/ch01.R
@@ -23,157 +23,9 @@ heatmap(gexp)
 
 saveRDS(gexp, "../data/ch01.rds")
 
-# Another go at simulating data
-# https://m-clark.github.io/docs/mixedModels/mixedModels.html#simulate_a_mixed_model
-set.seed(1)
-# Y_i = alpha_i + beta_i * X_i + epsilon_i
-n <- 6
-g <- 100
-alpha <- rnorm(g, mean = 10, sd = 1)
-epsilon <- rnorm(g, mean = 0, sd = 1)
+# Improved simulation ----------------------------------------------------------
 
-# DE high variance
-
-
-# non-DE high variance
-
-# DE low variance
-
-# non-DE low variance
-n <- 6
-g <- 100
-de <- logical(length = g)
-de[sample(seq_along(de), round(length(de) * .1))] <- TRUE
-alpha <- rnorm(g, mean = 10, sd = 1)
-epsilon <- matrix(rnorm(n * g, mean = 0, sd = 1),
-                  nrow = g, ncol = n)
-beta <- rnorm(g, mean = 0)
-beta[de] <- rnorm(sum(de), mean = 2)
-hist(beta)
-
-# Samples
-samples <- 1:6
-ns <- length(samples)
-status <- ifelse(samples <= ns / 2, "control", "case")
-table(status)
-# Genes
-g <- 1:100
-ng <- length(g)
-global_mean <- 10
-x <- matrix(global_mean, nrow = ng, ncol = ns)
-gene_specific_mean <- rnorm(ng, mean = 0, sd = 3)
-x <- sweep(x, 1, gene_specific_mean, FUN = "+")
-summary(rowMeans(x))
-hist(rowMeans(x))
-
-
-samples <- 1:6
-ns <- length(samples)
-status <- ifelse(samples <= ns / 2, 0, 1)
-
-set.seed(1)
-alpha <- rnorm(1, mean = 10, sd = 1)
-beta <- 0.5
-epsilon <- rnorm(ns, mean = 0, sd = 1)
-Yg <- alpha + beta * status + epsilon
-tapply(Yg, status, mean)
-mean(Yg[status == 1]) / mean(Yg[status == 0])
-t.test(Yg ~ status)
-
-log2(mean(Yg[status == 1]) / mean(Yg[status == 0]))
-boxplot(Yg ~ status)
-
-# 30 non-DE genes with high variance
-ng <- 30
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = 0, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 3)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-# 30 non-DE genes with low variance
-ng <- 30
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = 0, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 1)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-# 10 upregulated DE genes with low variance
-ng <- 10
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = 5, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 1)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-
-# 10 upregulated DE genes with high variance
-ng <- 10
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = 5, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 3)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-# 10 downregulated DE genes with low variance
-ng <- 10
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = -5, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 1)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-
-# 10 downregulated DE genes with high variance
-ng <- 10
-m <- matrix(NA, nrow = ng, ncol = ns)
-for (i in 1:ng) {
-  alpha <- rnorm(1, mean = 10, sd = 1)
-  beta <- rnorm(1, mean = -5, sd = 1)
-  epsilon <- rnorm(ns, mean = 0, sd = 3)
-  Yg <- alpha + beta * status + epsilon
-  m[i, ] <- Yg
-}
-image(t(m))
-
-m <- matrix(1:10, nrow = 10)
-status <- matrix(status, nrow = 1)
-
-
-samples <- rep(c(0, 1), each = 3)
-ns <- length(groups)
-genes <- 1:100
-ng <- length(genes)
-is_de <- logical(length = ng)
-is_de[sample(genes, round(ng / 2))] <- TRUE
-
-alpha <- rnorm(ng, mean = 10, sd = 1)
-beta <- matrix(rnorm(ng, mean = -50, sd = 1), ncol = 1)
-epsilon <- matrix(rnorm(ng * ns, mean = 0, sd = 30), nrow = ng, ncol = ns)
-Yg <- alpha + beta %*% status + epsilon
-image(t(Yg))
-
+set.seed(12345)
 create_exp_mat <- function(n1, n2, ng,
                            alpha_mean, beta_mean, epsilon_sd) {
   status <- c(rep(0, n1), rep(1, n2))
@@ -181,7 +33,7 @@ create_exp_mat <- function(n1, n2, ng,
   status <- matrix(status, nrow = 1)
 
   alpha <- rnorm(ng, mean = alpha_mean, sd = 1)
-  beta <- matrix(rnorm(ng, mean = beta_mean, sd = 2), ncol = 1)
+  beta <- matrix(rnorm(ng, mean = beta_mean, sd = 1), ncol = 1)
   epsilon <- matrix(rnorm(ng * ns, mean = 0, sd = epsilon_sd),
                     nrow = ng, ncol = ns)
   Yg <- alpha + beta %*% status + epsilon
@@ -193,12 +45,6 @@ x <- rbind(
   create_exp_mat(n1 = 3, n2 = 3, ng = 30, alpha_mean = 10, beta_mean = -1:1, epsilon_sd = 3),
   # 30 non-DE genes with low variance
   create_exp_mat(n1 = 3, n2 = 3, ng = 30, alpha_mean = 10, beta_mean = -1:1, epsilon_sd = 1),
-
-  # 10 low-effect up genes with low variance
-  create_exp_mat(n1 = 3, n2 = 3, ng = 10, alpha_mean = 10, beta_mean = 2, epsilon_sd = .75),
-  # 10 low-effect down genes with low variance
-  create_exp_mat(n1 = 3, n2 = 3, ng = 10, alpha_mean = 10, beta_mean = -2, epsilon_sd = .75),
-
   # 10 upregulated DE genes with low variance
   create_exp_mat(n1 = 3, n2 = 3, ng = 10, alpha_mean = 10, beta_mean = 5, epsilon_sd = 1),
   # 10 upregulated DE genes with high variance
