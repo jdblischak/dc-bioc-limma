7082aab8a938aa96b67ad021870b91fa3733a967
John Blischak <jdblischak@gmail.com>
2018-06-06 22:14:21 -0400

    Download and process raw Populus CEL files.

--- code/raw-affy.R
+++ code/raw-affy.R
@@ -12,16 +12,22 @@ library(Biobase)
 library(GEOquery)
 library(limma)
 
-# The Populus data provides raw CEL files generated from the platform:
+# Arabidopsis ------------------------------------------------------------------
+
+# The Arabidopsis data provides raw CEL files generated from the platform:
 #
 # GPL198 	[ATH1-121501] Affymetrix Arabidopsis ATH1 Genome Array
 #
 # ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE53nnn/GSE53990/suppl/GSE53990_RAW.tar
+#
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE53990
 
-getGEOSuppFiles(GEO = "GSE53990")
-untar("GSE53990/GSE53990_RAW.tar", exdir = "GSE53990")
+download <- getGEOSuppFiles(GEO = "GSE53990", baseDir = tempdir())
+tarfile <- rownames(download)
+untar(tarfile, exdir = dirname(tarfile))
 
-affybatch <- ReadAffy(filenames = Sys.glob("GSE53990/*CEL.gz"), compress = TRUE)
+affybatch <- ReadAffy(filenames = Sys.glob(file.path(dirname(tarfile), "*CEL.gz")),
+                      compress = TRUE)
 
 # Take the average of all the probes for a given gene. Do not do any other
 # pre-processing.
@@ -45,7 +51,52 @@ x <- normalizeBetweenArrays(x)
 
 plotDensities(x, legend = FALSE)
 
-x <- x[rowMeans(x) > 6, ]
+x <- x[rowMeans(x) > 5, ]
+
+plotDensities(x, legend = FALSE)
+
+dim(x)
+
+# Populus ----------------------------------------------------------------------
+
+# The Populus data provides raw CEL files generated from the platform:
+#
+# GPL4359	[Poplar] Affymetrix Poplar Genome Array
+#
+# https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE15242&format=file
+#
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE15242
+
+download <- getGEOSuppFiles(GEO = "GSE15242", baseDir = tempdir())
+tarfile <- rownames(download)
+untar(tarfile, exdir = dirname(tarfile))
+
+affybatch <- ReadAffy(filenames = Sys.glob(file.path(dirname(tarfile), "*CEL.gz")),
+                      compress = TRUE)
+
+# Take the average of all the probes for a given gene. Do not do any other
+# pre-processing.
+eset <- expresso(affybatch,
+                 bg.correct = FALSE,
+                 normalize = FALSE,
+                 pmcorrect.method = "pmonly",
+                 summary.method = "avgdiff")
+
+x <- exprs(eset)
+
+dim(x)
+
+plotDensities(x, legend = FALSE)
+
+x <- log(x)
+
+plotDensities(x, legend = FALSE)
+
+x <- normalizeBetweenArrays(x)
+
+plotDensities(x, legend = FALSE)
+
+x <- x[rowMeans(x) > 5, ]
 
 plotDensities(x, legend = FALSE)
 
