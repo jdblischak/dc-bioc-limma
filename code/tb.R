1c0504f3f9e9df3086a29157ce62987c210a61e0
John Blischak <jdblischak@gmail.com>
2018-06-13 15:39:48 -0400

    Improve visualization by focusing on time variable.

--- code/tb.R
+++ code/tb.R
@@ -1,87 +1,67 @@
-library("dplyr")
-library("edgeR")
-library("limma")
-library("Biobase")
+# Visualize and remove batch effects from TB data
+#
+# https://bitbucket.org/jdblischak/tb/src
 
-# Post batch correction --------------------------------------------------------
+library(Biobase)
+library(dplyr)
+library(limma)
+library(edgeR)
 
-x_raw_file <- "table-s1.txt"
-if (!file.exists(x_raw_file)) {
-  download.file(url = "https://bitbucket.org/jdblischak/tb-data/raw/bc0f64292eb8c42a372b3a2d50e3d871c70c202e/table-s1.txt",
-                destfile = x_raw_file)
+# Download data ----------------------------------------------------------------
+
+full_file <- "../data/counts_per_sample.txt"
+full_file_url <- "https://bitbucket.org/jdblischak/tb-data/raw/bc0f64292eb8c42a372b3a2d50e3d871c70c202e/counts_per_sample.txt"
+if (!file.exists(full_file)) {
+  download.file(url = full_file_url, destfile = full_file)
 }
-x_raw <- read.delim(x_raw_file, stringsAsFactors = FALSE)
 
-dim(x_raw)
-x <- x_raw %>% select(starts_with("M"))
-dim(x)
-rownames(x) <- x_raw$id
-
-f <- x_raw[, "name", drop = FALSE]
-rownames(f) <- x_raw$id
-
-p <- read.delim("https://bitbucket.org/jdblischak/tb-data/raw/bc0f64292eb8c42a372b3a2d50e3d871c70c202e/annotation.txt",
-                   stringsAsFactors = FALSE)
-dim(p)
-rownames(p) <- paste(p$ind, p$bact, p$time, sep = ".")
-colnames(x) <- rownames(p)
-
-eset <- ExpressionSet(assayData = as.matrix(x),
-                      phenoData = AnnotatedDataFrame(p),
-                      featureData = AnnotatedDataFrame(f))
-
-
-eset <- eset[, pData(eset)[, "bact"] %in% c("none", "Rv", "Salm") &
-               pData(eset)[, "time"] == 18]
-
-plotDensities(eset, legend = FALSE)
-plotMDS(eset, labels = pData(eset)[, "bact"], gene.selection = "common")
-plotMDS(eset, labels = pData(eset)[, "extr"], gene.selection = "common")
-
-# Pre batch correction ---------------------------------------------------------
+# Pre-process data -------------------------------------------------------------
 
 # Code adapted from https://bitbucket.org/jdblischak/tb/src/da33186481188c173f4d96fe8f4286cc3bbc1b9a/batch-correction-comparison.Rmd?at=master&fileviewer=file-view-default#batch-correction-comparison.Rmd-51
-full_file <- "counts_per_sample.txt"
-if (!file.exists(full_file)) {
-  download.file(url = "https://bitbucket.org/jdblischak/tb-data/raw/bc0f64292eb8c42a372b3a2d50e3d871c70c202e/counts_per_sample.txt",
-                destfile = full_file)
-}
+
 full <- read.delim(full_file, stringsAsFactors = FALSE)
 dim(full)
 full <- full[order(full$dir), ]
 rownames(full) <- paste(full$ind, full$bact, full$time, sep = ".")
-counts <- t(full[, grep("ENSG", colnames(full))])
+
+x <- t(full[, grep("ENSG", colnames(full))])
+p <- full %>% select(ind, bact, time, extr, rin)
+stopifnot(colnames(x) == rownames(p))
+
+eset <- ExpressionSet(assayData = x,
+                      phenoData = AnnotatedDataFrame(p))
+
 # Filter lowly expressed genes
-counts <- counts[rowSums(cpm(counts) > 1) > 6, ]
-dim(counts)
-stopifnot(colnames(counts) == rownames(p))
+keep <- rowSums(cpm(exprs(eset)) > 1) > 6
+sum(keep)
+eset <- eset[keep, ]
+dim(eset)
 
-eset <- ExpressionSet(assayData = counts,
-                      phenoData = AnnotatedDataFrame(p),
-                      featureData = AnnotatedDataFrame(f))
+norm_factors <- calcNormFactors(exprs(eset))
+exprs(eset) <- cpm(exprs(eset), lib.size = colSums(exprs(eset)) * norm_factors,
+                   log = TRUE)
 
-exprs(eset) <- cpm(eset, log = TRUE)
+pData(eset)[, "infection"] <- ifelse(pData(eset)[, "bact"] == "none",
+                                     "con", "inf")
 
-eset <- eset[, pData(eset)[, "bact"] %in% c("none", "Rv", "GC") &
-               pData(eset)[, "time"] == 18]
+pData(eset)[, "time"] <- ifelse(pData(eset)[, "time"] == 4,
+                                "early", "late")
+
+pData(eset)[, "batch"] <- sprintf("b%02d", pData(eset)[, "extr"])
+
+table(pData(eset)[, c("time", "batch")])
+
+# Analyze data -----------------------------------------------------------------
 
 plotDensities(eset, legend = FALSE)
-plotMDS(eset, labels = pData(eset)[, "bact"], gene.selection = "common")
-plotMDS(eset, labels = pData(eset)[, "extr"], gene.selection = "common")
+
+png("../figure/ch03/tb-pca-%03d.png")
+
 plotMDS(eset, labels = pData(eset)[, "time"], gene.selection = "common")
 
-pca <- prcomp(t(exprs(eset)), scale. = TRUE)
-plot(pca$x[, 1], pca$x[, 2], col = as.factor(pData(eset)[, "bact"]))
-plot(pca$x[, 1], pca$x[, 2], col = pData(eset)[, "time"])
-plot(pca$x[, 1], pca$x[, 2], col = pData(eset)[, "extr"])
+exprs(eset) <- removeBatchEffect(eset, batch = pData(eset)[, "batch"],
+                                 covariates = pData(eset)[, "rin"])
 
-library("ggplot2")
-theme_set(theme_classic())
-d <- cbind(pca$x, pData(eset))
-d$infection <- ifelse(d$bact == "none", "control", "infection")
-d$infection <- factor(d$infection, levels = c("infection", "control"))
-p <- ggplot(d, aes(x = PC1, y = PC2, color = as.factor(time), size = infection)) +
-  geom_point()
-p
-p + facet_wrap(~time)
-p %+% d[d$time == 18, ]
+plotMDS(eset, labels = pData(eset)[, "time"], gene.selection = "common")
+
+dev.off()
